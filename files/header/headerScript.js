78d2d19dcfd3c54f957c718e21e33632ac03face
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2024-12-08 18:15:22 -0800

    Update headerScript.js

--- files/header/headerScript.js
+++ files/header/headerScript.js
@@ -1,2 +1,6 @@
 const header = document.getElementById("header");
 const footer = document.getElementById("footer");
+
+header.innerHTML = "<div id='headerBkg'></div><div id='headerBar'><div id='hbContent'><img id='headerLogo' src='https://gcparksandresorts.github.io/careawayresort/files/images/careawayresort-logo.png'></div></div>";
+
+footer.innerHTML = "<img id='footerLogo' src='https://gcparksandresorts.github.io/careawayresort/files/images/careawayresort-logo-white.png'>";
