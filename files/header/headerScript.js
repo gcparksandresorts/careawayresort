c4295e410a996b56c5449e201af97b6179299c08
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2024-12-09 13:41:48 -0800

    Update headerScript.js

--- files/header/headerScript.js
+++ files/header/headerScript.js
@@ -1,12 +1,21 @@
 const header = document.getElementById("header");
 const footer = document.getElementById("footer");
+
+const pgHead = document.getElementById("pgHead");
 const loader = document.getElementById("loader");
 
 header.innerHTML = "<div id='headerBkg'></div><div id='headerBar'><div id='hbContent'><img id='headerLogo' src='https://gcparksandresorts.github.io/careawayresort/files/images/careawayresort-logo.png'></div></div>";
 
 footer.innerHTML = "<img id='footerLogo' src='https://gcparksandresorts.github.io/careawayresort/files/images/careawayresort-logo-white.png'>";
 
+
+// PAGE SETUP
 loader.innerHTML = "<img src='https://gcparksandresorts.github.io/careawayresort/files/header/webfavicon.ico'><h2>Loading Page</h2>";
+
+pgHead.innerHTML += "<meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'> \
+			<link rel='stylesheet' href='https://gcparksandresorts.github.io/careawayresort/files/header/headerStyle.css'> \
+			 <link rel='stylesheet' href='https://gcparksandresorts.github.io/careawayresort/files/styleSheet.css'> \
+			<link rel='icon' type='image/x-icon' href='https://gcparksandresorts.github.io/careawayresort/files/header/webfavicon.ico'>";
 
 function finishLoad(){
 	var opacVal = 1;
