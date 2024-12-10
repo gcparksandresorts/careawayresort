0d2458416dd898f4c711696b8b4343d99188b16e
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2024-12-09 18:05:51 -0800

    Update headerScript.js

--- files/header/headerScript.js
+++ files/header/headerScript.js
@@ -4,8 +4,10 @@ const footer = document.getElementById("footer");
 const pgHead = document.getElementById("pgHead");
 const loader = document.getElementById("loader");
 
-header.innerHTML = "<div id='headerBkg'></div><div id='headerBar'><div id='hbContent'><img id='headerLogo' onclick='"+'window.open("https://gcparksandresorts.github.io/careawayresort/","_self")'+"'src='https://gcparksandresorts.github.io/careawayresort/files/images/careawayresort-logo.png'></div></div>";
+header.innerHTML = "<div id='headerBkg'></div><div id='headerBar'><div id='hbContent'><img id='headerLogo' onclick='"+'window.open("https://gcparksandresorts.github.io/careawayresort/","_self")'+"'src='https://gcparksandresorts.github.io/careawayresort/files/images/careawayresort-logo.png'> \
+			 <div id='hbLinks'> <a>Home</a> <a>Things to Do</a> <a>Places to Stay</a> <a>Information</a> <a class='btn filled'>Tickets & Passes</a> </div> </div></div>";
 
+			// FOR SMALLER SCREENS: <a>☰</a>
 
 
 // FOOTER SETUP
