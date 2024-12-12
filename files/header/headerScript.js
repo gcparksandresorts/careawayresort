241cb10ae5d3d260dd81158c0378c86b57a3e228
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2024-12-12 14:12:19 -0800

    Update headerScript.js

--- files/header/headerScript.js
+++ files/header/headerScript.js
@@ -8,11 +8,12 @@ const siteLink = "https://gcparksandresorts.github.io/careawayresort/"; //   "+s
 
 header.innerHTML = "<div id='headerBkg'></div><div id='headerBar'><div id='hbContent'><img id='headerLogo' onclick='"+'window.open("https://gcparksandresorts.github.io/careawayresort/","_self")'+"'src='https://gcparksandresorts.github.io/careawayresort/files/images/careawayresort-logo.png'> \
 			 <div id='hbLinks'> <a class='bigMen'>&#11206; Things to Do</a> <a class='bigMen'>Places to Stay</a> <a class='bigMen'>Events</a> <a class='bigMen'>&#11206; Information</a> <a class='btn filled bigMen'>Tickets & Passes</a> \
-				<a class='miniMen' onclick='openMinMen(0)'>☰</a> </div> </div></div> \
-				<div id='hbSideMenu'><a class='btn' href='"+siteLink+"tickets'>Tickets and Passes</a> \
+				<a class='miniMen' onclick='openSideMenu()'>☰</a> </div> </div></div> \
+			<div id='hbSideMenu' class=''><a class='btn' href='"+siteLink+"tickets'>Tickets and Passes</a> \
 				<a onclick='openMinMen(1)'>&#11206; Things to Do</a> <div id='hbIntd1'> <a>Destinations</a> <a>Attractions & Entertainment</a> <a>Restaurants and Dining</a> <a>Shopping</a> </div> \
 				<a><span style='opacity:0'>&#11206;</span> Places to Stay</a> <a><span style='opacity:0'>&#11206;</span> Special Events</a> \
-				<a onclick='openMinMen(2)'>&#11206; Information</a> <div id='hbIntd2'> <a>Operating Schedule</a> <a>GC VIP Admission</a> <a>Annual Passports</a> <a>Safety Guidelines</a> </div> </div>";
+				<a onclick='openMinMen(2)'>&#11206; Information</a> <div id='hbIntd2'> <a>Operating Schedule</a> <a>GC VIP Admission</a> <a>Annual Passports</a> <a>Safety Guidelines</a> </div> </div> \
+			<div id='hbSideMenuCloser' onclick='openSideMenu()'></div> ";
 
 
 // FOOTER SETUP
@@ -38,7 +39,7 @@ function openMinMen(num){
         var tempDoc = 'Nan';
         switch(num){
 		case 0:
-			tempDoc = document.getElementById("hbSideMenu");  
+			// holding
 		break;
             	case 1:
         		tempDoc = document.getElementById("hbIntd1");  
@@ -54,3 +55,6 @@ function openMinMen(num){
                	tempDoc.style.display = 'block';
         }
 }// end of function openMinMen
+
+// Open or close the side menu by adding or removing the class that moves it to the right of the screen
+function openSideMenu(){document.getElementById("hbSideMenu").classList.toggle("open"); document.getElementById("hbSideMenuCloser").classList.toggle("open");}
