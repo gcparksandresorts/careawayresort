8897a1c1053dd45cb501795beaa88f64e0a4f6d5
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2024-12-12 17:02:00 -0800

    Update headerScript.js

--- files/header/headerScript.js
+++ files/header/headerScript.js
@@ -7,13 +7,14 @@ const loader = document.getElementById("loader");
 const siteLink = "https://gcparksandresorts.github.io/careawayresort/"; //   "+siteLink+"   //
 
 header.innerHTML = "<div id='headerBkg'></div><div id='headerBar'><div id='hbContent'><img id='headerLogo' onclick='"+'window.open("https://gcparksandresorts.github.io/careawayresort/","_self")'+"'src='https://gcparksandresorts.github.io/careawayresort/files/images/careawayresort-logo.png'> \
-			 <div id='hbLinks'> <a class='bigMen' id='ttdOpen' onclick='openTTDbarMenu()'>&#11206; Things to Do</a> <a class='bigMen'>Places to Stay</a> <a class='bigMen'>Events</a> <a class='bigMen' id='moreInfoBtn'>&#11206; Information</a> <a class='btn filled bigMen' href=' "+siteLink+"tickets'>Tickets & Passes</a> \
+			 <div id='hbLinks'> <a class='bigMen' id='ttdOpen' onclick='toggleBarMenu(1)'>&#11206; Things to Do</a> <a class='bigMen'>Places to Stay</a> <a class='bigMen'>Events</a> <a class='bigMen' id='moreInfoBtn' onclick='toggleBarMenu(2)'>&#11206; Information</a> <a class='btn filled bigMen' href=' "+siteLink+"tickets'>Tickets & Passes</a> \
 				<a class='miniMen' onclick='openSideMenu()'>☰</a> </div> </div></div> \
 			<div id='hbSideMenu' class=''><a class='btn' href='"+siteLink+"tickets'>Tickets and Passes</a> \
 				<a onclick='document.getElementById("+'"hbIntd1"'+").classList.toggle("+'"open"'+");'>&#11206; Things to Do</a> <div id='hbIntd1' class='hbSMsubMen'> <a>Destinations</a> <a>Attractions & Entertainment</a> <a>Restaurants and Dining</a> <a>Shopping</a> </div> \
 				<a><span style='opacity:0'>&#11206;</span> Places to Stay</a> <a><span style='opacity:0'>&#11206;</span> Special Events</a> \
 				<a onclick='document.getElementById("+'"hbIntd2"'+").classList.toggle("+'"open"'+");'>&#11206; Information</a> <div id='hbIntd2' class='hbSMsubMen'> <a>Operating Schedule</a> <a>GC VIP Admission</a> <a>Annual Passports</a> <a>Safety Guidelines</a> </div> </div> \
-			<div id='hbSideMenuCloser' onclick='openSideMenu()'></div> ";
+			<div id='hbSideMenuCloser' onclick='openSideMenu()'></div> \
+			<div id='ttdbarMenu' class='barMenu'> <a class='btn'>Attractions & Entertainment</a> <a class='btn'>Restaurants & Dining</a> <a class='btn'>Shopping</a> </div>";
 
 
 // FOOTER SETUP
@@ -37,12 +38,25 @@ function finishLoad(){
 // Open or close the side menu by adding or removing the class that moves it to the right of the screen
 function openSideMenu(){document.getElementById("hbSideMenu").classList.toggle("open"); document.getElementById("hbSideMenuCloser").classList.toggle("open");}
 
-function openTTDbarMenu(){
-	ttdButton = document.getElementById("ttdOpen");
-	ttdButton.classList.toggle("isSelected");
-	
-	moreInfoButton = document.getElementById("moreInfoBtn");
-	moreInfoButton.classList.remove("isSelected");
+function toggleBarMenu(num){
 
-	console.log("open ttdMenu");
+	ttdButton = document.getElementById("ttdOpen");
+	moreInfoButton = document.getElementById("moreInfoBtn");
+
+	ttdbarMenu = document.getElementById("ttdbarMenu");
+	
+	
+	if(num==1){
+		ttdButton.classList.toggle("isSelected");
+		moreInfoButton.classList.remove("isSelected");
+		ttdbarMenu.classList.toggle("open");
+	}else if(num==2){
+		ttdButton.classList.remove("isSelected");
+		moreInfoButton.classList.add("isSelected");
+		ttdbarMenu.classList.remove("open");
+	}else{
+		ttdButton.classList.remove("isSelected");
+		moreInfoButton.classList.remove("isSelected");
+		ttdbarMenu.classList.remove("open");
+	}
 }
