9ac219034e7d0b67a8595966d6cf67aa85742b39
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2024-12-13 19:25:24 -0800

    Update headerScript.js

--- files/header/headerScript.js
+++ files/header/headerScript.js
@@ -27,7 +27,8 @@ loader.innerHTML = "<img src='https://gcparksandresorts.github.io/careawayresort
 
 pgHead.innerHTML += "<meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'> \
 			<link rel='stylesheet' href='https://gcparksandresorts.github.io/careawayresort/files/header/headerStyle.css'> \
-			 <link rel='stylesheet' href='https://gcparksandresorts.github.io/careawayresort/files/styleSheet.css'> \
+			<link rel='stylesheet' href='https://gcparksandresorts.github.io/careawayresort/files/styleSheet.css'> \
+			<link href='https://fonts.googleapis.com/css?family=Funnel+Sans|Lexend+Deca' rel='stylesheet'>\
 			<link rel='icon' type='image/x-icon' href='https://gcparksandresorts.github.io/careawayresort/files/header/webfavicon.ico'>";
 
 // Function to close the loader after the page is fully loaded
