47c7a5619856dcacead8ce81cdc80270d3673bb5
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-01-04 15:31:37 -0800

    Create attractions.js

---
+++ files/repositories/attractions.js
@@ -0,0 +1,31 @@
+attractionList = []; attractionCategories = []; attractionLocations = []; attractionAge = [];
+val iter = 0;
+
+// Tabs 2 No wrap
+
+clickLink = "https://gcparksandresorts.github.io/careawayresort/things-to-do/attractions/";
+
+attractionCategories = ["none", "Thrilling Rollercoasters", "Immersive Dark Rides", "Family Friendly Attractions", "Interactive Experiences", "Live Entertainment", "Nighttime Spectaculars", "Other"];
+											// 0							1													2															3														4													5											6`										7
+
+attractionLocations = ["none", "Globe-City's Careaway Park", "Globe-City Port of Adventure", "Globe-City's Splashport Bay", "Shops & More District"];
+											// 0							1															2																	3															4													
+
+attractionAge = ["none", "For all ages", 'Minimum Height 35" (89cm)', 'Minimum Height 40" (102cm)', 'Minimum Height 45" (115cm)', 'Minimum Height 48" (122cm)', 'Minimum Height 50" (127cm)', 'Mature Themes (PG13)', 'Other']
+								// 0						1												2													3															4												5															6														7												8
+
+/*
+  iter+=1;
+  attractionList[iter] = "attractionName - sponsorText | attractionLocation | attractionCategory | attractionAge | clickLink | imageLink";
+*/
+
+	attractionList[iter] = "Temple of Medusa - none | 1 | 2 | 2 | careaway-park/temple-of-medusa | https://themeparkmagazine.com/wp-content/uploads/2023/07/Indiana-Jones-5.png";
+
+	iter+=1;
+  attractionList[iter] = "The Magic Bakery Ride - none | 1 | 2 | 1 | careaway-park/magic-bakery-ride | https://aztechadmit.github.io/gcresorthighland/main/images/banner2.jpg";
+
+	iter+=1;
+  attractionList[iter] = "Lightspeed - Sponsored by Volkswagen | 1 | 1 | 5 | careaway-park/lightspeed | https://static.wikia.nocookie.net/tron/images/7/77/Clu2LtCycl01.jpg/revision/latest?cb=20110827053101";
+
+	iter+=1;
+  attractionList[iter] = "Mission 14 - Sponsored by Kracken Eyewear | 1 | 1 | 3 | careaway-park/mission-14 | https://insidethemagic.net/wp-content/uploads/2021/02/Space-Mountain-Ride.jpg";
