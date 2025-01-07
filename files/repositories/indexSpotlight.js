d3681c6b6b4b1f7a392b105a37d962bc8ccbb4ad
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-01-07 12:13:40 -0800

    Create indexSpotlight.js

---
+++ files/repositories/indexSpotlight.js
@@ -0,0 +1,19 @@
+indexSpotlight = [];
+var ind = 0;
+
+// ind += 1; indexSpotlight[ind] = "month | heading | text_content | click_link | image_link";
+
+// January
+indexSpotlight[ind] = "January | A year's worth of adventure | Start the new year with Adventure by purchsing a Careaway Resort annual passport for 12 months of theme park fun! | tickets/annual-passports | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/tts3.jpg";
+ind += 1; indexSpotlight[ind] = "January | Clearence Sales starting now | As the holidays move past, visit some of your favorite stores around the resort for limited-time clearence sales and large discounts on merchandise and apparel | destinations/shops-and-more-district | https://www.universalstudioshollywood.com/tridiondata/ush/en/us/files/images/universal-citywalk-shopping-802x535.jpg";
+ind += 1; indexSpotlight[ind] = "January | Closed for refurbishment | Know before you go: some attractions may be closed for refurbishment. Download the GC RESORT mobile app to view attraction closures before your visit | information/gc-resort-app | https://allears.net/wp-content/uploads/2021/01/2021-Universal-Island-of-Adventure-Amazing-Adventures-of-Spider-Man-Entrance-Refurbishment-2-700x490.jpg";
+
+// February
+ind += 1; indexSpotlight[ind] = "February | heading | text_content | click_link | image_link";
+ind += 1; indexSpotlight[ind] = "February | heading | text_content | click_link | image_link";
+ind += 1; indexSpotlight[ind] = "February | Closed for refurbishment | Know before you go: some attractions may be closed for refurbishment. Download the GC RESORT mobile app to view attraction closures before your visit | information/gc-resort-app | https://allears.net/wp-content/uploads/2021/01/2021-Universal-Island-of-Adventure-Amazing-Adventures-of-Spider-Man-Entrance-Refurbishment-2-700x490.jpg";
+
+// March
+ind += 1; indexSpotlight[ind] = "March | heading | text_content | click_link | image_link";
+ind += 1; indexSpotlight[ind] = "March | heading | text_content | click_link | image_link";
+ind += 1; indexSpotlight[ind] = "March | heading | text_content | click_link | image_link";
