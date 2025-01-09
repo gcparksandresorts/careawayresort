cd268daa4561410432ab65316db554c64703b435
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-01-08 18:18:43 -0800

    Update attractions.js

--- files/repositories/attractions.js
+++ files/repositories/attractions.js
@@ -5,27 +5,37 @@ var iter = 0;
 
 attractionClickLink = "https://gcparksandresorts.github.io/careawayresort/things-to-do/attractions/";
 
-attractionCategories = ["none", "Thrilling Rollercoasters", "Immersive Dark Rides", "Family Friendly Attractions", "Interactive Experiences", "Live Entertainment", "Nighttime Spectaculars", "Other"];
-											// 0							1													2															3														4													5											6`										7
-
 attractionLocations = ["none", "Globe-City's Careaway Park", "Globe-City Port of Adventure", "Globe-City's Splashport Bay", "Shops & More District"];
-											// 0							1															2																	3															4													
+			// 0		1				2			3				4
+
+attractionCategories = ["none", "Thrill Rides", "Immersive Dark Rides", "Family Friendly Attractions", "Interactive Experiences", "Live Entertainment", "Nighttime Spectaculars", "Seasonal Attractions", "Seasonal Entertainment", "Other"];
+			// 0		 1		2			3				4			5			6`			7			8		9
 
 attractionAge = ["none", "For all ages", 'Minimum Height 35" (89cm)', 'Minimum Height 40" (102cm)', 'Minimum Height 45" (115cm)', 'Minimum Height 48" (122cm)', 'Minimum Height 50" (127cm)', 'Mature Themes (PG13)', 'Other']
-								// 0						1												2													3															4												5															6														7												8
+			// 0			 1				2			3				4				5				6`		7
 
 /*
-  iter+=1;
-  attractionList[iter] = "attractionName - sponsorText | attractionLocation | attractionCategory | attractionAge | clickLink | imageLink";
+iter+=1;
+attractionList[iter] = "attractionName - sponsorText | attractionLocation | attractionCategory | attractionAge | clickLink | imageLink";
 */
 
-	attractionList[iter] = "Temple of Medusa - none | 1 | 2 | 2 | careaway-park/temple-of-medusa | https://themeparkmagazine.com/wp-content/uploads/2023/07/Indiana-Jones-5.png";
+attractionList[iter] = "Temple of Medusa - none | 1 | 2 | 2 | careaway-park/temple-of-medusa | https://themeparkmagazine.com/wp-content/uploads/2023/07/Indiana-Jones-5.png";
 
-	iter+=1;
-  attractionList[iter] = "The Magic Bakery Ride - none | 1 | 2 | 1 | careaway-park/magic-bakery-ride | https://aztechadmit.github.io/gcresorthighland/main/images/banner2.jpg";
+iter+=1;
+attractionList[iter] = "The Magic Bakery Ride - none | 1 | 2 | 1 | careaway-park/magic-bakery-ride | https://aztechadmit.github.io/gcresorthighland/main/images/banner2.jpg";
 
-	iter+=1;
-  attractionList[iter] = "Lightspeed - Sponsored by Volkswagen | 1 | 1 | 5 | careaway-park/lightspeed | https://static.wikia.nocookie.net/tron/images/7/77/Clu2LtCycl01.jpg/revision/latest?cb=20110827053101";
+iter+=1;
+attractionList[iter] = "Lightspeed - Sponsored by General Electric | 1 | 1 | 5 | careaway-park/lightspeed | https://static.wikia.nocookie.net/tron/images/7/77/Clu2LtCycl01.jpg/revision/latest?cb=20110827053101";
 
-	iter+=1;
-  attractionList[iter] = "Mission 14 - Sponsored by Kracken Eyewear | 1 | 1 | 3 | careaway-park/mission-14 | https://insidethemagic.net/wp-content/uploads/2021/02/Space-Mountain-Ride.jpg";
+iter+=1;
+attractionList[iter] = "Mission 14 - Sponsored by Kracken Eyewear | 1 | 1 | 3 | careaway-park/mission-14 | https://insidethemagic.net/wp-content/uploads/2021/02/Space-Mountain-Ride.jpg";
+
+iter+=1;
+attractionList[iter] = "The Nightmare Dracula | 2 | 1 | 4 | port-of-adventure/nightmare-dracula | https://i.imgur.com/Iu4oHYO.jpeg";
+
+iter+=1;
+attractionList[iter] = "Frankenkoaster - Sponsored by Duracel<sup>TM</sup> | 2 | 1 | 5 | port-of-adventure/frankencoaster | https://i.imgur.com/RwM26qC.jpeg";
+
+iter+=1;
+attractionList[iter] = "Action! On the Backlot - Sponsored by Audi | 2 | 1 | 2 | port-of-adventure/action-backlot | https://i.imgur.com/wsSggBe.jpeg";
+
