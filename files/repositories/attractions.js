506bfc74e2b73799dbe6d47fc09c357a7e8c1a4e
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-01-08 18:28:08 -0800

    Update attractions.js

--- files/repositories/attractions.js
+++ files/repositories/attractions.js
@@ -31,11 +31,17 @@ iter+=1;
 attractionList[iter] = "Mission 14 - Sponsored by Kracken Eyewear | 1 | 1 | 3 | careaway-park/mission-14 | https://insidethemagic.net/wp-content/uploads/2021/02/Space-Mountain-Ride.jpg";
 
 iter+=1;
-attractionList[iter] = "The Nightmare Dracula | 2 | 1 | 4 | port-of-adventure/nightmare-dracula | https://i.imgur.com/Iu4oHYO.jpeg";
+attractionList[iter] = "The Nightmare Dracula - none | 2 | 1 | 4 | port-of-adventure/nightmare-dracula | https://i.imgur.com/Iu4oHYO.jpeg";
 
 iter+=1;
 attractionList[iter] = "Frankenkoaster - Sponsored by Duracel<sup>TM</sup> | 2 | 1 | 5 | port-of-adventure/frankencoaster | https://i.imgur.com/RwM26qC.jpeg";
 
 iter+=1;
 attractionList[iter] = "Action! On the Backlot - Sponsored by Audi | 2 | 1 | 2 | port-of-adventure/action-backlot | https://i.imgur.com/wsSggBe.jpeg";
+
+iter+=1;
+attractionList[iter] = "Wonder Reel Studio Tour - none | 2 | 4 | 0 | port-of-adventure/studio-tour | https://i.imgur.com/ZMFbfYk.jpeg";
+
+iter+=1;
+attractionList[iter] = "World of Ghibli - Sponsored by Aztech Cinema | 1 | 3 | 2 | careaway-park/world-of-ghibli | https://images.adsttc.com/media/images/6246/e74c/3e4b/31d6/6800/0004/newsletter/A2275-15.jpg?1648813895";
 
