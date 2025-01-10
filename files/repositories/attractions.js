df0efa7c3b486c7b3fffd80d912ca0b16b5afb9f
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-01-10 13:15:38 -0800

    Update attractions.js

--- files/repositories/attractions.js
+++ files/repositories/attractions.js
@@ -12,7 +12,7 @@ attractionCategories = ["none", "Thrill Rides", "Immersive Dark Rides", "Family 
 			// 0		 1		2			3				4			5			6`			7			8		9
 
 attractionAge = ["none", "For all ages", 'Minimum Height 35" (89cm)', 'Minimum Height 40" (102cm)', 'Minimum Height 45" (115cm)', 'Minimum Height 48" (122cm)', 'Minimum Height 50" (127cm)', 'Mature Themes (PG13)', 'Other']
-			// 0			 1				2			3				4				5				6`		7
+		// 0		 1			2			3				4				5				6`			7
 
 /*
 iter+=1;
@@ -34,14 +34,22 @@ iter+=1;
 attractionList[iter] = "The Nightmare Dracula - none | 2 | 1 | 4 | port-of-adventure/nightmare-dracula | https://i.imgur.com/Iu4oHYO.jpeg";
 
 iter+=1;
-attractionList[iter] = "Frankenkoaster - Sponsored by Duracel<sup>TM</sup> | 2 | 1 | 5 | port-of-adventure/frankencoaster | https://i.imgur.com/RwM26qC.jpeg";
+attractionList[iter] = "Frankenkoaster - Sponsored by Duracel<sup>TM</sup> | 2 | 1 | 6 | port-of-adventure/frankencoaster | https://i.imgur.com/RwM26qC.jpeg";
 
 iter+=1;
 attractionList[iter] = "Action! On the Backlot - Sponsored by Audi | 2 | 1 | 2 | port-of-adventure/action-backlot | https://i.imgur.com/wsSggBe.jpeg";
 
 iter+=1;
-attractionList[iter] = "Wonder Reel Studio Tour - none | 2 | 4 | 0 | port-of-adventure/studio-tour | https://i.imgur.com/ZMFbfYk.jpeg";
+attractionList[iter] = "Wonder Reel Studio Tour - none | 2 | 4 | 1 | port-of-adventure/studio-tour | https://i.imgur.com/ZMFbfYk.jpeg";
 
 iter+=1;
 attractionList[iter] = "World of Ghibli - Sponsored by Aztech Cinema | 1 | 3 | 2 | careaway-park/world-of-ghibli | https://images.adsttc.com/media/images/6246/e74c/3e4b/31d6/6800/0004/newsletter/A2275-15.jpg?1648813895";
 
+iter+=1;
+attractionList[iter] = "Looney's Musical Train - Sponsored by Dole | 1 | 2 | 1 | careaway-park/looneys-musical-train | https://s7d2.scene7.com/is/image/TWCNews/1203_n13_disney_mickey_minnie_runaway_railway";
+
+iter+=1;
+attractionList[iter] = "Careaway Castle Mini Golf - none | 4 | 4 | 1 | clickLink | https://blog-cdn.touringplans.com/blog/wp-content/uploads/2021/04/hollywoodriveingolf_sperduto-700x465.jpeg";
+
+iter+=1;
+attractionList[iter] = "Enchanted Forest to Creature Cove Gondola - none | 1 | 3 | 1 | clickLink | https://www.disneytouristblog.com/wp-content/uploads/2019/06/hollywood-studios-skyliner-gondolas-disney-world-2215.jpg";
