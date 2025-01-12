59bf79a38a246e3623d8412212f2fa611d18b230
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-01-12 10:31:11 -0800

    Create dateTier.js

---
+++ files/repositories/dateTier.js
@@ -0,0 +1,11 @@
+// Date Tier File holds all information for the Resort Operations Schedule
+
+// PRICING AND DAY TIERS
+	/* 
+		Tiers range from 1 - 8
+  		Each day tier is determined by the operating hours of resort attractions and targeted guest count
+
+    		TIER 1 - Off Peak General Operations
+      		
+ 	*/
+	
