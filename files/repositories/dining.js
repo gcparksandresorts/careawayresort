e67b5202177cbbcf1ae3b1e7d320d663b204da05
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-01-13 13:31:43 -0800

    Create dining.js

---
+++ files/repositories/dining.js
@@ -0,0 +1,27 @@
+diningList = []; diningCategories = []; diningLocations = []; mealStyle = [];
+var iter = 0;
+
+// Tabs 2 No wrap
+
+diningClickLink = "https://gcparksandresorts.github.io/careawayresort/things-to-do/dining/";
+
+diningLocations = ["none", "Globe-City's Careaway Park", "Globe-City Port of Adventure", "Globe-City's Splashport Bay", "Shops & More District"];
+			// 0		1				2			3				4
+
+diningCategories = ["none", "Casual Dining", "Counter Service", "Full Service", "Grab & Go"];
+		//   0		1		2			3		4
+
+mealStyle = ["none", "Breakfast", "Lunch", "Dinner", "Snacks & Beverages"];
+	//    0		1	    2		3	4
+/*
+iter+=1;
+diningList[iter] = "restaurantName | Description | diningLocation | diningCategory | mealStyle1,mealStyle2 | clickLink | imageLink";
+*/
+
+diningList[iter] = "Character Cafe | Description | 1 | 2 | 1,2 | clickLink | imageLink";
+
+iter+=1;
+diningList[iter] = "Cosmic Cantina | Take a break and fuel up with some of the best grub in the galaxy. | 1 | 2 | 1,2 | clickLink | imageLink";
+
+iter+=1;
+diningList[iter] = "Yetti Yum | oo yummy | 2 | 4 | 2,4 | clickLink | imageLink";
