4186ea67534b3bde2696971c9164bb3a4ffdd63d
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-01-14 10:15:10 -0800

    Update entertainment.js

--- files/repositories/entertainment.js
+++ files/repositories/entertainment.js
@@ -1,15 +1,22 @@
+entertainmentClickLink = "https://gcparksandresorts.github.io/careawayresort/things-to-do/entertainment/";
+
 entertainmentList = []; entertainmentCategory = []; entertainmentLocation = []; entertainmentSeason = [];
 
-entertainmentCategory = ["none", "Live Entertainment", "Nighttime Spectacular", "Parade / Cavalcade"];
+entertainmentCategory = ["none", "Live Entertainment", "Nighttime Spectacular", "Parade / Cavalcade", "Theater Show / Performance", "Interactive Improv"];
+			// 0		1			2			3			4			5
 
 entertainmentLocation = ["none", "Globe-City's Careaway Park", "Globe-City Port of Adventure", "Globe-City's Splashport Bay", "Shops & More District"];
 			// 0		1				2			3				4
 
-entertainmentSeason = ["none", "Fall", "Winter", "Spring", "Summer"];
-			// 0	1	2  	   3		4
+entertainmentSeason = ["none", "Fall", "Winter", "Spring", "Summer", "Always Happening"];
+			// 0	1	2  	   3		4	5
 var iter = 0;
 
 /*
 iter += 1;
-entertainmentList[iter] = "showCode0 | showName1 | showSubtext2 | showHours3 | showLocation4 | showCategory5 | showSeason6 | clickLink7 | imgLink8";
+entertainmentList[iter] = "showCode0 | showName1 | showDescription2 | showDisclaimer3 | showHours4 | showLocation5 | showCategory6 | showSeason7 | clickLink8 | imgLink9";
 */
+
+
+// OFF-PEAK / GENERIC ENTERTAINMENT
+entertainmentList[iter] = "GCMB | Globe-City Marching Band | A loud and proud high-rolling group | showDisclaimer3 | 10:00 AM, 12:00 PM, 2:00 PM, 4:00 PM | 1 | 1 | 5 | clickLink8 | https://www.widener.edu/sites/default/files/2019-07/marching-band.jpg";
