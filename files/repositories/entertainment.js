7b6b50852c13a6eaf3a479c7e79cade0ae0e2d52
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-01-14 09:27:46 -0800

    Create entertainment.js

---
+++ files/repositories/entertainment.js
@@ -0,0 +1,15 @@
+entertainmentList = []; entertainmentCategory = []; entertainmentLocation = []; entertainmentSeason = [];
+
+entertainmentCategory = ["none", "Live Entertainment", "Nighttime Spectacular", "Parade / Cavalcade"];
+
+entertainmentLocation = ["none", "Globe-City's Careaway Park", "Globe-City Port of Adventure", "Globe-City's Splashport Bay", "Shops & More District"];
+			// 0		1				2			3				4
+
+entertainmentSeason = ["none", "Fall", "Winter", "Spring", "Summer"];
+			// 0	1	2  	   3		4
+var iter = 0;
+
+/*
+iter += 1;
+entertainmentList[iter] = "showCode | showName | showSubtext | showHours | showSeason | clickLink | imgLink";
+*/
