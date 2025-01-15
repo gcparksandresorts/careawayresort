b8bce59d1d92adb5471ae7761a86126f9ee9bef0
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-01-15 10:42:59 -0800

    Update attractions.js

--- files/repositories/attractions.js
+++ files/repositories/attractions.js
@@ -27,9 +27,10 @@ iter+=1; attractionList[iter] = "Lightspeed - Sponsored by General Electric | 1 
 iter+=1; attractionList[iter] = "Looney's Musical Train - Sponsored by Dole | 1 | 2 | 1 | clickLink | https://s7d2.scene7.com/is/image/TWCNews/1203_n13_disney_mickey_minnie_runaway_railway";
 iter+=1; attractionList[iter] = "Mission 14 - Sponsored by Kracken Eyewear | 1 | 1 | 3 | clickLink | https://insidethemagic.net/wp-content/uploads/2021/02/Space-Mountain-Ride.jpg";
 iter+=1; attractionList[iter] = "Starfield Horizons - Sponsored by Unity3D | 1 | 3 | 2 | clickLink | https://ca-times.brightspotcdn.com/dims4/default/e46afb6/2147483647/strip/true/crop/4928x3280+0+0/resize/1200x799!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fb2%2Fb8%2F6ae2faf94bdba46e6fd1b98188e4%2F1209zv-0320msa.jpg";
-iter+=1; attractionList[iter] = "Temple of Medusa - none | 1 | 2 | 2 | clickLink | https://themeparkmagazine.com/wp-content/uploads/2023/07/Indiana-Jones-5.png";
+iter+=1; attractionList[iter] = "Temple of Medusa - none | 1 | 2 | 2 | clickLink | https://gcparksandresorts.github.io/careawayresort/files/images/rides/medussa1.webp";
 iter+=1; attractionList[iter] = "The Magic Bakery Ride - none | 1 | 2 | 1 | clickLink | https://aztechadmit.github.io/gcresorthighland/main/images/banner2.jpg";
 iter+=1; attractionList[iter] = "The Nightmare Dracula - none | 2 | 1 | 4 | clickLink | https://i.imgur.com/Iu4oHYO.jpeg";
+iter+=1; attractionList[iter] = "The Quest of Odysseus - none | 2 | 2 | 2 | clickLink | https://gcparksandresorts.github.io/careawayresort/files/images/rides/odyseus1.webp";
 iter+=1; attractionList[iter] = "Voyage to Atlantia - A rebuild of the #1 Globe-City darkride! | 2 | 2 | 2 | clickLink | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/atlantisConcept4.png";
 iter+=1; attractionList[iter] = "Wonder Reel Studio Tour - none | 2 | 4 | 1 | clickLink | https://i.imgur.com/ZMFbfYk.jpeg";
 iter+=1; attractionList[iter] = "World of Ghibli - Sponsored by Aztech Cinema | 1 | 3 | 2 | clickLink | https://images.adsttc.com/media/images/6246/e74c/3e4b/31d6/6800/0004/newsletter/A2275-15.jpg?1648813895";
