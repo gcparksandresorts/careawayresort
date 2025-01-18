1db44cb28c36d4c49b9edebb772f47d9a071580b
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-01-18 14:17:25 -0800

    Update dining.js

--- files/repositories/dining.js
+++ files/repositories/dining.js
@@ -8,16 +8,19 @@ diningClickLink = "https://gcparksandresorts.github.io/careawayresort/things-to-
 diningLocations = ["none", "Globe-City's Careaway Park", "Globe-City Port of Adventure", "Globe-City's Splashport Bay", "Shops & More District"];
 			// 0		1				2			3				4
 
-diningCategories = ["none", "Casual Dining", "Counter Service", "Full Service", "Grab & Go"];
-		//   0		1		2			3		4
+diningCategories = ["none", "Casual Dining", "Counter Service", "Full Service", "Grab & Go", "Kiosk Ordering", "Poolside Dining"];
+		//   0		1		2			3		4  	   5			6
 
 mealStyle = ["none", "Breakfast", "Lunch", "Dinner", "Snacks & Beverages"];
-	//    0		1	    2		3	4
+	//    0		1	    2		3		4
 /*
 iter+=1;
 diningList[iter] = "restaurantName | Description | diningLocation | diningCategory | mealStyle1,mealStyle2 | clickLink | imageLink";
 */
 
+diningList[iter] = "Careaway Proper | Endulge in classic minecraftian eats and lay-back into the relaxing and comforting atmosphere of vacation | 1 | 3 | 1,2,3 | clickLink | https://media.nbclosangeles.com/2023/07/disneylandjuly26.jpg?quality=85&strip=all&resize=1200%2C675";
+
+iter+=1;
 diningList[iter] = "Character Cafe | Description | 1 | 2 | 1,2 | clickLink | imageLink";
 
 iter+=1;
@@ -25,3 +28,4 @@ diningList[iter] = "Cosmic Cantina | Take a break and fuel up with some of the b
 
 iter+=1;
 diningList[iter] = "Yetti Yum | oo yummy | 2 | 4 | 2,4 | clickLink | imageLink";
+
