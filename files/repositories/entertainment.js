263d35be68c1168813f991dab52372ecfce1008e
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-01-20 21:08:10 -0800

    Update entertainment.js

--- files/repositories/entertainment.js
+++ files/repositories/entertainment.js
@@ -32,3 +32,7 @@ var iter = 0;
 // HOLIDAY SEASON ENTERTAINMENT
 	iter += 1; entertainmentList[iter] = "iss | Ignite the Season Spectacular | A cheery fireworks, projections, and light spectacular featuring classic holiday magic | showDisclaimer3 | 9:30 PM | 2 | 2 | 2 | clickLink8 | https://media.wdwnt.com/2022/11/Believe-in-Holiday-Magic-Fireworks.jpg";
 	iter += 1; entertainmentList[iter] = "fol | Fountains of Light | A dazzling watershow celebrating the magic of the holiday season | showDisclaimer3 | 9:00 PM, 10:30 PM | 1 | 2 | 2 | clickLink8 | https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/1600/900/75/dam/disneyland/entertainment/disney-california-adventure/world-of-color-season-of-light/holidays-8-hr-duratex-16x9.jpg?1736238112292";
+
+// SPRING AND SUMMER SEASON ENTERTAINMENT
+	iter += 1; entertainmentList[iter] = "wick | Wicked! The Retelling | An abridged remake of the world-famous musical based on the book based on the movie based on a really old book. Live from the Sasson Theater at Wonder Reel Studios! | showDisclaimer3 | 12:45 PM, 4:15 PM | 2 | 4 | 4 | clickLink8 | https://gcparksandresorts.github.io/careawayresort/files/images/shows/wickedMusical.webp";
+	iter += 1; entertainmentList[iter] = "das | Dreams of Adventure Spectacular | An epic nighttime fireworks and light show about the magic of dreams and the places they take us | showDisclaimer3 | 9:30 PM | 2 | 2 | 4 | clickLink8 | https://blogmickey.com/wp-content/uploads/2017/05/HappilyEverAfter-0512017-14.jpg";
