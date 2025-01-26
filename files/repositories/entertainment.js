8efd682460fe3ca7a2a3ff08ef97851199549941
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-01-25 16:39:42 -0800

    Update entertainment.js

--- files/repositories/entertainment.js
+++ files/repositories/entertainment.js
@@ -18,6 +18,8 @@ var iter = 0;
 
 /*
 	iter += 1; entertainmentList[iter] = "showCode0 | showName1 | showDescription2 | showDisclaimer3 | showHours4 | showLocation5 | showCategory6 | showSeason7 | clickLink8 | imgLink9";
+
+ 	To link to to the main page with index use: entIndex=NUM where NUM is the index
 */
 
 
@@ -34,5 +36,5 @@ var iter = 0;
 	iter += 1; entertainmentList[iter] = "fol | Fountains of Light | A dazzling watershow celebrating the magic of the holiday season | showDisclaimer3 | 9:00 PM, 10:30 PM | 1 | 2 | 2 | clickLink8 | https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/1600/900/75/dam/disneyland/entertainment/disney-california-adventure/world-of-color-season-of-light/holidays-8-hr-duratex-16x9.jpg?1736238112292";
 
 // SPRING AND SUMMER SEASON ENTERTAINMENT
-	iter += 1; entertainmentList[iter] = "wick | Wicked! The Retelling | An abridged remake of the world-famous musical based on the book based on the movie based on a really old book. Live from the Sasson Theater at Wonder Reel Studios! | showDisclaimer3 | 12:45 PM, 4:15 PM | 2 | 4 | 4 | clickLink8 | https://gcparksandresorts.github.io/careawayresort/files/images/shows/wickedMusical.webp";
+	iter += 1; entertainmentList[iter] = "wick | Wicked! The Retelling | An abridged remake of the world-famous musical based on the book based on the movie based on a really old book. Live from the Sasson Theater at Wonder Reel Studios! | showDisclaimer3 | 12:45 PM, 4:15 PM | 2 | 4 | 4 | entIndex=1 | https://gcparksandresorts.github.io/careawayresort/files/images/shows/wickedMusical.webp";
 	iter += 1; entertainmentList[iter] = "das | Dreams of Adventure Spectacular | An epic nighttime fireworks and light show about the magic of dreams and the places they take us | showDisclaimer3 | 9:30 PM | 2 | 2 | 4 | clickLink8 | https://blogmickey.com/wp-content/uploads/2017/05/HappilyEverAfter-0512017-14.jpg";
