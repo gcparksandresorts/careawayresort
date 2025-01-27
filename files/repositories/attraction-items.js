3959ba6b1ce4085683afd57a65a33afb948f9d18
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-01-26 16:08:18 -0800

    Create attraction-items.js

---
+++ files/repositories/attraction-items.js
@@ -0,0 +1,8 @@
+attractionItems = [];
+attractionDescrition = [];
+attractionImg = [];
+
+queueText = ["Standby queue only", "<a href='../../tickets/gcvip'>Priority Queuing</a> available", "<a href='../../tickets/gcvip'>Priority Queuing</a> and <a href='../../information/single-riders'>Single Riders</a> available"];
+		// 0			1 (VIP only)								2 (VIP and single)
+attractionAge = ["none", "For all ages", 'Minimum Height 35" (89cm)', 'Minimum Height 40" (102cm)', 'Minimum Height 45" (115cm)', 'Minimum Height 48" (122cm)', 'Minimum Height 50" (127cm)', 'Mature Themes (PG13)', 'Other']
+		// 0		 1			2			3				4				5				6`			7
