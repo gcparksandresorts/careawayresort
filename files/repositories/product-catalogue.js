2d399e3f80eb556f59d30ab77ae2f5ebf51fb55d
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-01-27 14:37:21 -0800

    Create product-catalogue.js

---
+++ files/repositories/product-catalogue.js
@@ -0,0 +1,31 @@
+productCategories = ["none", "Men & Womens Apparel", "Accessories", "Theme Park & Resort Gifts", "Toys & Collectibles"];
+
+productList=[];
+productImg = [];
+productDescription = [];
+
+// COLOR LIST
+// List the hex codes IE #efefef,#990990, etc.
+// Only list one color if necessary
+// If color choice not available -> none
+
+// SIZE LIST -> yes / no
+// List sizes: Small, Medium, Large, XLarge, XXLarge
+// Other options for size: List Quantities
+
+// Product Categories:
+// All-0   Apparel-1	Accessories-2	Themepark Gifts-3	Toys/Collectibles-4
+
+/*
+productList[NUM]="productName0 | category1 | price2 | colorList3,colorList | sizeList4,sizeList";
+	productImg[NUM] = "imgLink";
+	productDescription[NUM] = "shortDescription | longerDescription";
+*/
+
+productList[0]="Mr. Teddy's Cafe Crewneck Sweatshirt | 1 | 64.50 | #2E6F40,#d6b588,#422701 | Small, Medium, Large, XLarge";
+	productImg[0] = "https://i.imgur.com/RTTXXNw.jpeg";
+	productDescription[0] = "Stay warm with this cozy cotton crewneck and always be reminded of your favorite furry Barista, Mr. Teddy! | Mr. Teddy is still brewing the rest of this description.";
+
+productList[1]="Careaway Resort Crewneck Sweatshirt | 1 | 64.50 | #000080,#82c8e5 | Small, Medium, Large, XLarge, XXLarge";
+	productImg[1] = "https://i.imgur.com/9LQCTqY.png";
+	productDescription[1] = "The official Careaway Resort sweatshirt. Formal on the front, party on the back. | longerDescription";
