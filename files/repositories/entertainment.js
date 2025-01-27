1367e73b6f4f51ec783fcc788f57c9678af02828
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-01-27 12:04:44 -0800

    Update entertainment.js

--- files/repositories/entertainment.js
+++ files/repositories/entertainment.js
@@ -29,11 +29,11 @@ var iter = 0;
 
 // HALLOWEEN SEASON ENTERTAINMENT
 	iter += 1; entertainmentList[iter] = "koda | Kingdom of Darkness Alive | A spooky special effects performance where the world of the dead comes to the living | 2 | Hourly beggining at 8PM | 2 | 6 | 1 | entIndex=3 | https://www.wfla.com/wp-content/uploads/sites/71/2018/01/the-nighttime-lights-at-hogwarts-castle_36661926_ver1.0.jpg";
-	iter += 1; entertainmentList[iter] = "haho | Haunting Hollywood | Horror and humor combine in this raunchy halloween retelling of some of the year's hottest gossip | 2 | 8:15 PM, 10:15 PM | 2 | 4 | 1 | clickLink8 | https://i.ytimg.com/vi/Phbnc10RXo4/maxresdefault.jpg";
+	iter += 1; entertainmentList[iter] = "haho | Haunting Hollywood | Horror and humor combine in this raunchy halloween retelling of some of the year's hottest gossip | 2 | 8:15 PM, 10:15 PM | 2 | 4 | 1 | entIndex=5 | https://i.ytimg.com/vi/Phbnc10RXo4/maxresdefault.jpg";
 
 // HOLIDAY SEASON ENTERTAINMENT
 	iter += 1; entertainmentList[iter] = "iss | Ignite the Season Spectacular | A cheery fireworks, projections, and light spectacular featuring classic holiday magic | showDisclaimer3 | 9:30 PM | 2 | 2 | 2 | clickLink8 | https://media.wdwnt.com/2022/11/Believe-in-Holiday-Magic-Fireworks.jpg";
-	iter += 1; entertainmentList[iter] = "fol | Fountains of Light | A dazzling watershow celebrating the magic of the holiday season | showDisclaimer3 | 9:00 PM, 10:30 PM | 1 | 2 | 2 | clickLink8 | https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/1600/900/75/dam/disneyland/entertainment/disney-california-adventure/world-of-color-season-of-light/holidays-8-hr-duratex-16x9.jpg?1736238112292";
+	iter += 1; entertainmentList[iter] = "fol | Fountains of Light | A dazzling watershow celebrating the magic of the holiday season | showDisclaimer3 | 9:00 PM, 10:30 PM | 1 | 2 | 2 | entIndex=6 | https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/1600/900/75/dam/disneyland/entertainment/disney-california-adventure/world-of-color-season-of-light/holidays-8-hr-duratex-16x9.jpg?1736238112292";
 
 // SPRING AND SUMMER SEASON ENTERTAINMENT
 	iter += 1; entertainmentList[iter] = "wick | Wicked! The Retelling | An abridged remake of the world-famous musical based on the book based on the movie based on a really old book. Live from the Sasson Theater at Wonder Reel Studios! | showDisclaimer3 | 12:45 PM, 4:15 PM | 2 | 4 | 4 | entIndex=1 | https://gcparksandresorts.github.io/careawayresort/files/images/shows/wickedMusical.webp";
