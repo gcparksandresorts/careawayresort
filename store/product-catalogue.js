5cfa529b8e8ebde143e0924bddf8f7721358dfcb
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-01-28 10:36:21 -0800

    Update product-catalogue.js

--- store/product-catalogue.js
+++ store/product-catalogue.js
@@ -38,19 +38,19 @@ productList[3]="Careaway Drive Novelty Road Sign | 3 | 40.00 | #0000CD | 6x30 in
 	productImg[3] = "https://i.imgur.com/Aiac3fm.jpeg";
 	productDescription[3] = "Serving as the first 'sign' that you've arrived at the Careaway Resort | Take home that feeling of excitement upon turning onto the official Careaway Drive, you've made it!";
 
-productList[4]="Splashport Bay Men's Swim / Board Shorts | 1 | $43.65 | #000080,#2E8B57,#FF4500 | Youth Medium,Youth Large,Adult Small,Adult Medium,Adult Large,Adult XLarge";
+productList[4]="Splashport Bay Men's Swim / Board Shorts | 1 | 43.65 | #000080,#2E8B57,#FF4500 | Youth Medium,Youth Large,Adult Small,Adult Medium,Adult Large,Adult XLarge";
 	productImg[4] = "https://flogrown.com/cdn/shop/products/FGSH-006-F_1.jpg?v=1682013894";
 	productDescription[4] = "High-quality above-knee swim trunks (board shorts) with printed design. | Get ready to splash with these awesome board shorts. The design is printed into the material meaning it won't fade away or deterioriate, even under the wettest conditions!";
 
-productList[5]="Careaway C Adjustable Structured Hat | 1 | $29.99 | #000000,#F0FFFF | Standard";
+productList[5]="Careaway C Adjustable Structured Hat | 1 | 29.99 | #000000,#F0FFFF | Standard";
 	productImg[5] = "https://i.imgur.com/RvYJZvQ.png";
 	productDescription[5] = "Embroidered structured brim hat snap adjustable | Stay cool in the shade or hide a messy hair day with this sleek hat. The famous Careaway C is a semi-incognido method of sporting your favorite resort's merch without being too loud about it.";
 
-productList[5]="Splashport Bay Adjustable Structured Hat | 1 | $29.99 | #000000,#F0FFFF | Standard";
-	productImg[5] = "https://i.imgur.com/SGwImBn.png";
-	productDescription[5] = "Embroidered structured brim hat snap adjustable | Don't let the kracken see you! Hide your face without hiding your love for Puertas Nuevas's favorite water park, oh and protect your face from the sun too or whatever.";
+productList[6]="Splashport Bay Adjustable Structured Hat | 1 | 29.99 | #000000,#F0FFFF | Standard";
+	productImg[6] = "https://i.imgur.com/SGwImBn.png";
+	productDescription[6] = "Embroidered structured brim hat snap adjustable | Don't let the kracken see you! Hide your face without hiding your love for Puertas Nuevas's favorite water park, oh and protect your face from the sun too or whatever.";
 
-productList[6]="Sipping Adventure Careaway Resort 22oz Tumbler | 2 | $39.99 | #F0FFFF,#000080 | 22oz";
-	productImg[6] = "https://cdn.4imprint.com/prod/extras/154054/547741/700/6.jpg";
-	productDescription[6] = "Vacuum sealed insulated tumbler for hot or cold drinks | <h4>Special Offer</h4>Bring your Careaway Resort tumbler to any dining location for 5% off your drinks! <p>Stay hydrated, and caffinated, with this sleek tumbler. The single-color laser-engraved design will ensure you stay classy while recounting your favorite adventure-filled memories.</p>";
+productList[7]="Sipping Adventure Careaway Resort 22oz Tumbler | 2 | 31.99 | #F0FFFF,#000080 | 22oz";
+	productImg[7] = "https://cdn.4imprint.com/prod/extras/154054/547741/700/6.jpg";
+	productDescription[7] = "Vacuum sealed insulated tumbler for hot or cold drinks | <h4>Special Offer</h4>Bring your Careaway Resort tumbler to any dining location for 5% off your drinks! <p>Stay hydrated, and caffinated, with this sleek tumbler. The single-color laser-engraved design will ensure you stay classy while recounting your favorite adventure-filled memories.</p>";
 
