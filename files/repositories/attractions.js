002deea7d15da05ce94b3aeb367e7cf4c0ea99fb
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-01-30 09:35:09 -0800

    Update attractions.js

--- files/repositories/attractions.js
+++ files/repositories/attractions.js
@@ -21,7 +21,7 @@ link to pages: atIndex=NUM
 */
 
 attractionList[iter] = "Action! On the Backlot - Sponsored by Audi | 2 | 1 | 2 | atIndex=1 | https://i.imgur.com/wsSggBe.jpeg";
-iter+=1; attractionList[iter] = "Careaway Castle Mini Golf - none | 4 | 4 | 1 | clickLink | https://blog-cdn.touringplans.com/blog/wp-content/uploads/2021/04/hollywoodriveingolf_sperduto-700x465.jpeg";
+iter+=1; attractionList[iter] = "Careaway Castle Mini Golf - Tickets required | 4 | 4 | 1 | clickLink | https://blog-cdn.touringplans.com/blog/wp-content/uploads/2021/04/hollywoodriveingolf_sperduto-700x465.jpeg";
 iter+=1; attractionList[iter] = "Enchanted Forest to Creature Cove Gondola - none | 1 | 3 | 1 | clickLink | https://www.disneytouristblog.com/wp-content/uploads/2019/06/hollywood-studios-skyliner-gondolas-disney-world-2215.jpg";
 iter+=1; attractionList[iter] = "Escape from Realinu - none | 1 | 2 | 2 | clickLink | https://miro.medium.com/v2/da:true/resize:fit:1200/0*w5Zv7N4WakVhjhKn";
 iter+=1; attractionList[iter] = "Frankenkoaster - Sponsored by Duracel<sup>TM</sup> | 2 | 1 | 6 | clickLink | https://i.imgur.com/RwM26qC.jpeg";
@@ -36,3 +36,5 @@ iter+=1; attractionList[iter] = "The Quest of Odysseus - none | 2 | 2 | 2 | clic
 iter+=1; attractionList[iter] = "Voyage to Atlantia - A rebuild of the #1 Globe-City darkride! | 2 | 2 | 2 | clickLink | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/atlantisConcept4.png";
 iter+=1; attractionList[iter] = "Wonder Reel Studio Tour - none | 2 | 4 | 1 | atIndex=2 | https://i.imgur.com/ZMFbfYk.jpeg";
 iter+=1; attractionList[iter] = "World of Ghibli - Sponsored by Aztech Cinema | 1 | 3 | 2 | atIndex=3 | ../files/images/banners/ghibliArt.webp";
+iter+=1; attractionList[iter] = "8Lanes Bowling Alley and Sports Bar - Pay per game | 4 | 4 | 1 | 8lanes | https://gcparksandresorts.github.io/careawayresort/files/images/banners/wavesAlley.jfif";
+
