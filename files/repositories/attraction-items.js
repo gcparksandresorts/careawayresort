8dadc5ad18625a4e9ae20a71457c219f4eea6231
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-02-02 23:11:27 -0800

    Update attraction-items.js

--- files/repositories/attraction-items.js
+++ files/repositories/attraction-items.js
@@ -2,8 +2,8 @@ attractionItems = [];
 attractionDescrition = [];
 attractionImg = [];
 
-queueText = ["Standby queue only", "<a href='../../tickets/gcvip'>Priority Queuing</a> available", "<a href='../../tickets/gcvip'>Priority Queuing</a> and <a href='../../information/single-riders'>Single Riders</a> available"];
-		// 0			1 (VIP only)								2 (VIP and single)
+queueText = ["Standby queue only", "<a href='../../tickets/gcvip'>Priority Queuing</a> available", "<a href='../../tickets/gcvip'>Priority Queuing</a> and <a href='../../information/single-riders'>Single Riders</a> available", "Not Applicable"];
+		// 0			1 (VIP only)				2 (VIP and single)		3 (not applicable)
 
 accessibilityInfo = ["Guest may remain in wheelchair or ECV", "Guest must transfer from wheelchair", "Some vehicles can accomodate standard wheelchairs"];
 	//		0						1				2
@@ -32,7 +32,7 @@ attractionItems[3] = "World of Ghibli/Overlock+SC | 3 | Creature's Cove,1 | 4 Mi
 attractionItems[2] = "Wonder Reel Studio Tour/Delius | 4 | Wonder Reel Studios,2 | 6 Minutes | 1 | 1 | 2 | yes";
 	attractionDescrition[2] = "A scatterbrained director has accidentally lost dozens of film reels around the studio and it’s up to you to save the day. Hop aboard a studio-owned golf cart equipped with a barcode scanner and search through vibrant movie sets. Your mission? Spot and scan the hidden reels to help the director finish his blockbuster masterpiece. | Interactive,Cinema,Family Fun,Movie Making,Cutsey | \
 				Lights, camera, action! With surprises around every corner, larger-than-life props, and plenty of Hollywood magic, this thrilling ride puts you right in the heart of the action. Whether you’re a film buff or just love a good adventure, the Wonder Reel Studio Tour Ride promises lights, laughs, and unforgettable memories for all ages!";
-	attractionImg[2] = "../../ | otherImg1 - otherImg";
+	attractionImg[2] = "../../files/images/banners/wonderReelStudioTour.webp | otherImg1 - otherImg";
 
 attractionItems[0] = "rideName0/font | rideCategory1 | rideLocation2,parkIndex | rideDuration3 | queueIndex4 | minHeight5 | accessibility6 | photoPassYN7";
 	attractionDescrition[0] = "shortIntro0 | tags1,otherTag | longParagraph2";
@@ -43,3 +43,7 @@ attractionItems[1] = "Action! On the Backlot/Bevan | 1 | Wonder Reel Studios,2 |
 				Ready for his next blockbuster, famed director Joshua Owens is looking to amp up the action. To perfectly capture his cinematic vision, Owens has employed the use of Stuntmobiles, high tech vehicles capable of capturing the narliest and actioniest shots. <p>Participate in the movies by riding in one of the high-tech stuntmobiles and experience the action of filmaking for yourself!</p>\
 				<p><h3>Know Before You Go</h3><em>Action! On the Backlot</em> is a thrilling simulator dark-ride that employs the use of fire, water, fog, and strobe effects. This ride features simulated motion, sporadic movement, fast turns and drops. Expectant mothers, and those with conditions which may be agrivated by this experience should not ride.</p>";
 	attractionImg[1] = "https://www.rwsentosa.com/-/jssmedia/project/non-gaming/rwsentosa/attractions/universal-studios-singapore/rides/uss-sci-fi-transformers-750-x-422.jpg?mw=1200 | otherImg1 - otherImg";
+
+attractionItems[4] = "The CozmiBotz Return/font | 3 | Galaxy Center,1 | 8 Minutes | 3 | minHeight5 | 0 | no";
+	attractionDescrition[4] = "Following a long journey from Highland, Oras Nou, the CosmiBotz are ready for their next grand performance! | Musical,Animatronics,Performance,Experience,Querky | Delight in the sounds of <em>Baroque Hoedown</em> as the CosmiBotz fill the galaxy with energy and music.";
+	attractionImg[4] = "../../files/images/banners/kookieBand.webp | otherImg1 - otherImg";
