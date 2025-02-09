51ab44fbc9460f9ba87256cb988949f0fd24dd6d
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-02-09 11:46:39 -0800

    Update collapsible.js

--- files/scripts/collapsible.js
+++ files/scripts/collapsible.js
@@ -4,7 +4,5 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    content.classList.toggle = "opened";
   });
 }
