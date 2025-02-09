2344f172e8b4a44d4d4acb5d8a91d395d93a57f3
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-02-09 15:29:30 -0800

    Update FAQ-questionsList.js

--- files/repositories/FAQ-questionsList.js
+++ files/repositories/FAQ-questionsList.js
@@ -1,8 +1,8 @@
 questionList = [];
 answerList = [];
 
-categoryList = ["none", "General", "Theme Parks", "Resort Hotels", "Annual Passports", "GC VIP", "Accessibility"];
-//                0        1            2              3                4                 5          6
+categoryList = ["none", "General", "Theme Parks", "Resort Hotels", "Annual Passports", "GC VIP", "Accessibility", "Transportation", "Merchandise & Online Store"];
+//                0        1            2              3                4                 5          6                  7                              8
 
 /*
 iter+=1;  questionList[iter] = "category | question";
@@ -12,6 +12,9 @@ var iter = 0;
 
 questionList[iter] = "5 | How do I redeem the complimentary GC PhotoPass"; 
           answerList[iter] = "GC VIP Deluxe and Platinum admission and upgrades include complimentary GC PhotoPass for unlimited downloads of all your ride photos and in-park photos. To access this feature, download the GC RESORT mobile app, press 'Link Tickets' then scan the barcode on the back of your admission tickets. Once linked, visit the GC PhotoPass module to link ride photos, linking your ticket should automatically unlock access to this feature.";
+
+iter+=1;  questionList[iter] = "7 | Are the Careaway-Express Shuttles free?";
+          answerList[iter] = "Careaway Express shuttle services around the resort, including to and from affiliate hotels, is complimentary. The only exception is shuttle service to and from the Puertas Nuevas International Airport (PNI) which is $25 per person.";
 
 iter+=1;  questionList[iter] = "5 | If I upgrade my admission to GC VIP, can I upgrade again to a higher tier later?";
           answerList[iter] = "Yes, all admission tiers are eligible to upgrade to a higher tier of GCVIP privileges. However, certain tiers may be sold-out during your visit. Consult Guest Services or any in-park information booths for details.";
@@ -24,3 +27,12 @@ iter+=1;  questionList[iter] = "5 | How do I know how much time is left on my co
 
 iter+=1;  questionList[iter] = "5 | What if I lose my GC VIP paper ticket?";
           answerList[iter] = "Once your ticket is printed, we encourage you to download the GC RESORT mobile app and link your tickets right away! This way you will always have a digital version of your ticket. If you lose your paper ticket, they can be reprinted at any in-park information booth, guest services, or Will Call. Either your paper ticket or your digital ticket are requried to enter the prirority queues and lounges.";
+
+iter+=1;  questionList[iter] = "8 | Can I order merchandise to be delivered to my hotel room?";
+          answerList[iter] = "Sort of. You can order your merchandise for 'pick-up' and select from a range of locations including the Sprucewood Lodge, The RW Hotel, and the World of Wonder hotel Luggage Services desks. Currently, these are the only hotels availabe for merchandise delivery. Merchandise will not be delivered directly to your room.";
+
+iter+=1;  questionList[iter] = "8 | What is the best way to return items I purchased online?";
+          answerList[iter] = "This applies for items eligible for standard return only. To avoid paying shipping fees, you can return items ordered online to any Globe-City Store (City-Shop Highland, Shops&More District). Alternatively, you can return items to our affiliate stores: LAB-Stores, RMember Clubhouses, R&R Logistics Centers.<p>If you can't visit in person, you can start an online return by sending an email to the customer service account shown on your order's digital reciept.</p>";
+
+iter+=1;  questionList[iter] = "8 | I got the wrong size - what do I do?";
+          answerList[iter] = "Within 30 days of recieving your online order, you can bring in your items and the reciept for the order to any Globe-City Store location to perform an exchange, assuming that store has the items in stock. If the store does not have the items, they will instead give you store credit equal to the value of your item (before tax).";
