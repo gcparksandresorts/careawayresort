8eeed163d97e49c9efe3d612e0f6a483f032a01e
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-02-19 10:23:45 -0800

    Update dining.js

--- files/repositories/dining.js
+++ files/repositories/dining.js
@@ -21,13 +21,10 @@ diningList[iter] = "restaurantName | Description | diningLocation | diningCatego
 diningList[iter] = "Careaway Proper | Endulge in classic minecraftian eats and lay-back into the relaxing and comforting atmosphere of vacation | 1 | 3 | 1,2,3 | clickLink | https://media.nbclosangeles.com/2023/07/disneylandjuly26.jpg?quality=85&strip=all&resize=1200%2C675";
 
 iter+=1;
-diningList[iter] = "Character Cafe | Description | 1 | 2 | 1,2 | clickLink | imageLink";
+diningList[iter] = "Character Cafe | Description | 1 | 2 | 1,2 | clickLink | https://i.pinimg.com/736x/d8/52/72/d85272cf9023831ef8f56dd411b7b6ff.jpg";
 
 iter+=1;
-diningList[iter] = "Cosmic Cantina | Take a space shuttle lightyears to the next galaxy top sip on some galactic cocktails and drinks that are out of this world | 1 | 1 | 4 | clickLink | https://cdn.discordapp.com/attachments/1309764417990426687/1337603383711891639/IMG_20250207_175739_508.jpg?ex=67a80bba&is=67a6ba3a&hm=24bf4aef1c73584c4f21c1feab28dcefd8b0f19fd6fee656796e04467bbaf384&";
-
-iter+=1;
-diningList[iter] = "Cosmic Cantina | Take a break and fuel up with some of the best grub in the galaxy. | 1 | 2 | 1,2 | clickLink | imageLink";
+diningList[iter] = "Cosmic Cantina | Take a space shuttle lightyears to the next galaxy top sip on some galactic cocktails and drinks that are out of this world | 1 | 1 | 4 | clickLink | https://i.imgur.com/Ovmkdyl.jpeg";
 
 iter+=1;
 diningList[iter] = "Mr. Teddy's Coffee Corner | Greet the cutsey creatures of Creature Cove and enjoy some simple sipping of delicious coffee from the famous bear himself | 1 | 4 | 0,4 | clickLink | https://img.freepik.com/premium-photo/teddy-bear-chair-coffee-shop-kid-room-empty-space-add-artwork-3d-rendering_1028938-222455.jpg";
