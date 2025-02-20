b6bf574d6d32dd2a73de46b8adb75b7aa20d26db
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-02-19 16:36:15 -0800

    Update dateTier.js

--- files/repositories/dateTier.js
+++ files/repositories/dateTier.js
@@ -1,11 +1,23 @@
 // Date Tier File holds all information for the Resort Operations Schedule
 
-// PRICING AND DAY TIERS
-	/* 
-		Tiers range from 1 - 8
-  		Each day tier is determined by the operating hours of resort attractions and targeted guest count
 
-    		TIER 1 - Off Peak General Operations
-      		
- 	*/
-	
+// TICKET TIERS AND HOURS
+	// tier[NUM] = "ticketTier0 | careawayHrs1,spEventHrs | portAdventHrs2,spEventHrs | shopsNMoreHrs3";
+	// if closed: write "closed", no special event: "none"
+	tier[0] = "ticketTier0 | careawayHrs1,spEventHrs | portAdventHrs2,spEventHrs | shopsNMoreHrs3 | splashportHrs4";
+
+	// ticketTier[NUM] = "tierNum0 | PtoP_upgrade1| resident2 | standard3 | vipBronze4 | vipSilver5 | vipDeluxe6 | vipPlatinum7 | splashPortStandard8 | splashPortPlatinum9 | splashPortPassholder10";
+	ticketTier[0] = "AUTO (1 or 2) Depending on Day of Week";
+	// Special events only
+
+
+	// SPECIAL EVENT TICKETS
+	// eventTier[NUM] = "eventName0 | ticketTypeName1 | tier1Price_2 | tier2Price_3 | tier3Price_4 | tier4Price_5 | tier5Price_6 | passHolderDis_7 | imgLink8";
+
+	eventTier[0] = "Scareaway Screams | General Admission | 50 | 65 | tier3Price_4 | tier4Price_5 | tier5Price_6 | 7 | imgLink8";
+	eventTier[1] = "Scareaway Screams | GCVIP Admission | 99 | 175 | tier3Price_4 | tier4Price_5 | tier5Price_6 | 0 | imgLink8";
+
+// RESORT SCHEDULE
+var iter = -1;
+
+	// iter+=1;dateTier[iter] = "dayWeek | MMM DD | tier2 | specialEventList3 | entertainmentList4 | pageBanner5";
