471c586f5e239ac9ec63bb8f45a74004a4327456
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-02-25 09:51:45 -0800

    Update indexSpotlight.js

--- files/repositories/indexSpotlight.js
+++ files/repositories/indexSpotlight.js
@@ -8,19 +8,23 @@ const webLink = 'https://gcparksandresorts.github.io/careawayresort/';
 // January
 indexSpotlight[ind] = "January | A year's worth of adventure | Start the new year with Adventure by purchsing a Careaway Resort annual passport for 12 months of theme park fun! | "+webLink+"tickets/annual-passports | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/tts3.jpg";
 ind += 1; indexSpotlight[ind] = "January | Clearence Sales starting now | As the holidays move past, visit some of your favorite stores around the resort for limited-time clearence sales and large discounts on merchandise and apparel | "+webLink+"destinations/shops-and-more-district | https://www.universalstudioshollywood.com/tridiondata/ush/en/us/files/images/universal-citywalk-shopping-802x535.jpg";
-ind += 1; indexSpotlight[ind] = "January | Closed for refurbishment | Know before you go: some attractions may be closed for refurbishment. Download the GC RESORT mobile app to view attraction closures before your visit | "+webLink+"information/gc-resort-app | https://i.imgur.com/4Xlk2uz.jpeg";
+ind += 1; indexSpotlight[ind] = "January | Closed for refurbishment | Know before you go: some attractions may be closed for refurbishment. Click here to view the referb calendar. | "+webLink+"information/blog/operations-update?index=7 | https://i.imgur.com/4Xlk2uz.jpeg";
 
 // February
 ind += 1; indexSpotlight[ind] = "February | Puertas Nuevas residents: save up to $200 on tickets | Residents with valid PNID can access exclusive offers on tickets to resort theme parks including 3-day, 1 park per day tickets as low as $53 per day | "+webLink+"tickets/resident-admission | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/tts3.jpg";
 ind += 1; indexSpotlight[ind] = "February | Affordable vacation made easy | During the off-season, enjoy exclusive vacation packages at competitive prices for up to 40% off standard pricing. Leave the planning to us, we'll leave the fun to you! | "+webLink+"hotels/vacation-packages | https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/1600/900/75/dam/wdpro-assets/dlr/places-to-stay/grand-californian/recreation/grand-californian-pool-family-16x9.jpg?1706116994236";
 ind += 1; indexSpotlight[ind] = "February | Valentines & Galentines Celebrations | It's the season of love, for significant others and for our besties. Celebrate with a care-free couple's retreat to the home of adventure! Click to view our list of the top 10 most romantic spots to profess your love at the Careaway Resort! | "+webLink+"information/blog/blog-post?index=4 | https://i.imgur.com/oSHVWEH.jpeg";
 ind += 1; indexSpotlight[ind] = "February | A year's worth of adventure | Feel unlimited. Purchase a Careaway Resort annual passport for 12 months of theme park fun! | "+webLink+"tickets/annual-passports | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/tts3.jpg";
-ind += 1; indexSpotlight[ind] = "February | Closed for refurbishment | Know before you go: some attractions may be closed for refurbishment. Download the GC RESORT mobile app to view attraction closures before your visit | "+webLink+"information/gc-resort-app | https://i.imgur.com/4Xlk2uz.jpeg";
+ind += 1; indexSpotlight[ind] = "February | Closed for refurbishment | Know before you go: some attractions may be closed for refurbishment. Click here to view the referb calendar. | "+webLink+"information/blog/operations-update?index=7 | https://i.imgur.com/4Xlk2uz.jpeg";
 
 // March
 ind += 1; indexSpotlight[ind] = "March | Puertas Nuevas residents: save up to $200 on tickets | Residents with valid PNID can access exclusive offers on tickets to resort theme parks including 3-day, 1 park per day tickets as low as $53 per day | "+webLink+"tickets/resident-admission | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/tts3.jpg";
 ind += 1; indexSpotlight[ind] = "March | Spring Break fun for everyone | As spring comes around, enjoy the sunshine with a day of fun. Explore special ticket pricing and more for an affordable vacation that's fun for everyone | "+webLink+"tickets | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/vipBanner1.jpg";
 ind += 1; indexSpotlight[ind] = "March | LaLa's Guide to fun in the sun | Whether its fun with family or cute spring dates or just-because, view this guide to maximize your fun in the sun in and out of the parks | "+webLink+"information/blog/fun-in-the-sun | https://i.imgur.com/danB9Na.jpeg";
+ind += 1; indexSpotlight[ind] = "March | Closed for refurbishment | Know before you go: some attractions may be closed for refurbishment. Click here to view the referb calendar. | "+webLink+"information/blog/operations-update?index=7 | https://i.imgur.com/4Xlk2uz.jpeg";
+
+// April
+
 
 // May
 ind += 1; indexSpotlight[ind] = "May | Celebrating Our Opening Day | Take a blast to the past and explore the hectic opening day of Globe-City's Careaway Resort | "+webLink+"information/blog/look-back-opening-day | https://i.imgur.com/pa18xQe.jpeg";
