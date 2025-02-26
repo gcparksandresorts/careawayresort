9a90f6bb9848da44a50e74b366dfddd5467bf3b8
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-02-26 10:48:50 -0800

    Update indexSpotlight.js

--- files/repositories/indexSpotlight.js
+++ files/repositories/indexSpotlight.js
@@ -14,16 +14,18 @@ ind += 1; indexSpotlight[ind] = "January | Closed for refurbishment | Know befor
 ind += 1; indexSpotlight[ind] = "February | Puertas Nuevas residents: save up to $200 on tickets | Residents with valid PNID can access exclusive offers on tickets to resort theme parks including 3-day, 1 park per day tickets as low as $53 per day | "+webLink+"tickets/resident-admission | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/tts3.jpg";
 ind += 1; indexSpotlight[ind] = "February | Affordable vacation made easy | During the off-season, enjoy exclusive vacation packages at competitive prices for up to 40% off standard pricing. Leave the planning to us, we'll leave the fun to you! | "+webLink+"hotels/vacation-packages | https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/1600/900/75/dam/wdpro-assets/dlr/places-to-stay/grand-californian/recreation/grand-californian-pool-family-16x9.jpg?1706116994236";
 ind += 1; indexSpotlight[ind] = "February | Valentines & Galentines Celebrations | It's the season of love, for significant others and for our besties. Celebrate with a care-free couple's retreat to the home of adventure! Click to view our list of the top 10 most romantic spots to profess your love at the Careaway Resort! | "+webLink+"information/blog/blog-post?index=4 | https://i.imgur.com/oSHVWEH.jpeg";
-ind += 1; indexSpotlight[ind] = "February | A year's worth of adventure | Feel unlimited. Purchase a Careaway Resort annual passport for 12 months of theme park fun! | "+webLink+"tickets/annual-passports | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/tts3.jpg";
+ind += 1; indexSpotlight[ind] = "February | A year's worth of adventure | Feel unlimited. Purchase a Careaway Resort annual passport for 12 months of theme park fun! | "+webLink+"tickets/annual-passports | https://i.imgur.com/w6T6Ege.jpeg";
 ind += 1; indexSpotlight[ind] = "February | Closed for refurbishment | Know before you go: some attractions may be closed for refurbishment. Click here to view the referb calendar. | "+webLink+"information/blog/operations-update?index=7 | https://i.imgur.com/4Xlk2uz.jpeg";
 
 // March
 ind += 1; indexSpotlight[ind] = "March | Puertas Nuevas residents: save up to $200 on tickets | Residents with valid PNID can access exclusive offers on tickets to resort theme parks including 3-day, 1 park per day tickets as low as $53 per day | "+webLink+"tickets/resident-admission | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/tts3.jpg";
 ind += 1; indexSpotlight[ind] = "March | Spring Break fun for everyone | As spring comes around, enjoy the sunshine with a day of fun. Explore special ticket pricing and more for an affordable vacation that's fun for everyone | "+webLink+"tickets | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/vipBanner1.jpg";
 ind += 1; indexSpotlight[ind] = "March | LaLa's Guide to fun in the sun | Whether its fun with family or cute spring dates or just-because, view this guide to maximize your fun in the sun in and out of the parks | "+webLink+"information/blog/fun-in-the-sun | https://i.imgur.com/danB9Na.jpeg";
+ind += 1; indexSpotlight[ind] = "March | A year's worth of adventure | Feel unlimited. Purchase a Careaway Resort annual passport for 12 months of theme park fun! | "+webLink+"tickets/annual-passports | https://i.imgur.com/w6T6Ege.jpeg";
 ind += 1; indexSpotlight[ind] = "March | Closed for refurbishment | Know before you go: some attractions may be closed for refurbishment. Click here to view the referb calendar. | "+webLink+"information/blog/operations-update?index=7 | https://i.imgur.com/4Xlk2uz.jpeg";
 
 // April
+ind += 1; indexSpotlight[ind] = "April | A year's worth of adventure | Feel unlimited. Purchase a Careaway Resort annual passport for 12 months of theme park fun! | "+webLink+"tickets/annual-passports | https://i.imgur.com/NCyozNl.jpeg";
 
 
 // May
