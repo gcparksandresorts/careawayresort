b74c5d40ce64380c78652e34ab604783aa945d67
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2025-03-05 14:56:50 -0800

    Update generateTicketCode.js

--- files/scripts/generateTicketCode.js
+++ files/scripts/generateTicketCode.js
@@ -18,13 +18,13 @@ function generateTicket(type,date,numDays,name,addOn){
 	}
 
 	let result = "GC"; // Initialize an empty string to store the result
-	name = name.toUpperCase().replace(/\s/g, '');
+	name = name.toUpperCase(); //.replace(/\s/g, '-')
 	
     	for (let i = 0; i < name.length; i++) {
-		if(name.charCodeAt(i) != 32){
+		if(name.charCodeAt(i) != '32'){
 		        let charCode = name.charCodeAt(i) - 64; // Get the ASCII code of each character
 		        result += charCode.toString().padStart(2, '0'); // Append the character code to the result string
-		}else{ result += "-";}
+		}else{ result += "00";} // Replace SPACE with 00
     	}
 
 	outputCode += result.toString();
@@ -37,4 +37,37 @@ function generateTicket(type,date,numDays,name,addOn){
 	console.log("Generated Code: " + outputCode);
 
 	return outputCode;
-}// end of function generateTicket(type,date,numDays,name)
+}// end of function generateTicket(type,date,numDays,name,addOn)
+
+function readTicket(ticketID){
+	// Split barcode at the GC
+	let temp = ticketID.split("GC");
+		let tempTwo = temp[1].split("AO"); // seperate name and addOns sections
+		let nameCode = tempTwo[0]; // name
+		let addOnCode = tempTwo[1]; // add on
+
+	// Get Ticket Type TT
+	let tikType = temp[0].substring(4,6);
+
+	// Get visit date (MM-DD)
+	let visDate = temp[0].substring(0,2) + "-" + temp[0].substring(2,4);
+	
+	// Decode Name
+	let nameVal = "";
+	for (let i = 0; i < nameCode.length; i += 2) {
+		let numStr = nameCode.substring(i, i + 2); // get two digits at a time
+		if(numStr != '00'){
+			let charCode = parseInt(numStr) + 64; // Convert number back to ASCII code
+			nameVal += String.fromCharCode(charCode); // Convert ASCII code to letter
+		}else{nameVal += " ";}
+	}
+
+	const resultsVal = [nameVal, tikType, visDate, addOnCode];
+
+	return resultsVal;
+}// end of function readTicket(ticketID)
+
+/*
+	console.clear;
+	console.log(generateTicket(1,"03-15",1,"Reuven Reyman","0"));
+*/
