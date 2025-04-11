55c8d2a8df703a4a756211ff4abe5d6568b92385
Reuven Reyman <reuvenreyman@gmail.com>
2025-04-11 00:45:18 -0700

    added search icon to header. expanded search capabilities to include all repositories.

--- files/repositories/siteIndex.js
+++ files/repositories/siteIndex.js
@@ -47,5 +47,62 @@ iter+=1; websiteIndex[iter] = {title: "Annual Passports",
     link: "tickets/annual-passports"
     };
 
+iter+=1; websiteIndex[iter] = {title: "Seasonal Events and Celebrations", 
+    description: "Information about seasonal events and celebrations at the Careaway Resort happening throughout the year", 
+    tagsList: "event,season,celebration,party,pride,holiday,celebrate,halloween,scary,scareaway,spooky,food,special,specials",
+    link: "things-to-do/events"
+    };
+
+iter+=1; websiteIndex[iter] = {title: "Rides & Attractions", 
+    description: "View information on the amazing rides and attractions at the resort from thrilling rollercoasters with sky-high adrenaline to immersive dark rides transporting you to mystical realms", 
+    tagsList: "ride,fun,attraction,thrill,adventure,rollercoaster,dark,darkride,immersive,interactive,experience,experiences,things,to,do,things-to-do,amusement,park,theme,themed,transport",
+    link: "things-to-do/attractions"
+    };
+
+iter+=1; websiteIndex[iter] = {title: "GCVIP Admission", 
+    description: "Information about deluxe admission including exclusive access to priority queuing, indoor lounges, and other perks", 
+    tagsList: "vip,tickets,ticket,park,themepark,admission,gc-vip,priority,queuing,queue,fast,fastpass,fast-pass,line,skip,skip-the-line",
+    link: "tickets/gc-vip"
+    };
+
+iter+=1; websiteIndex[iter] = {title: "Places to Stay / Hotels", 
+    description: "Information about hotels and lodging at the Careaway Resort including The Sprucewood Lodge, the RW, World of Wonder, MCR WAVES Resort, and MCR Port of Palms resort", 
+    tagsList: "hotel,bed,accomodations,resort,stay,lodging,rooms,room,stay,overnight,overnight-stay,check,check-in,check-out",
+    link: "hotels/home"
+    };
+
+iter+=1; websiteIndex[iter] = {title: "Safety Guidelines", 
+    description: "Rules and information about safety and security at the resort including ride safety, park rules, and security", 
+    tagsList: "safe,rule,information,info,severe,weather,regulation,legal,rides",
+    link: "information/safety-guidelines"
+    };
+
+iter+=1; websiteIndex[iter] = {title: "Corporate Partners", 
+    description: "Learn about our sponsors and the companies who help keep the Careaway Resort running", 
+    tagsList: "business,sponsor,company,partner,partnership,advertise,advertising,advertisement",
+    link: "business/corporate-partners"
+    }; 
+
+iter+=1; websiteIndex[iter] = {title: "Resort Blog & Operations Updates", 
+    description: "View all the current news and updates from the resort including blog posts, operating updates, and more", 
+    tagsList: "operation,news,media,update,article,articles,post,social",
+    link: "information/blog"
+    }; 
+
+iter+=1; websiteIndex[iter] = {title: "Online Store", 
+    description: "Shop for exclusive Careaway Resort merchandise and souvenirs online", 
+    tagsList: "gift,merch,retail,order,shopping,gifts",
+    link: "store/home"
+    }; 
+
+iter+=1; websiteIndex[iter] = {title: "pageTitle", 
+    description: "pageDescription", 
+    tagsList: "tag1,tag2,tag3",
+    link: "pageLink"
+    }; 
+
+
+    
+
 
 
--- files/scripts/searchEngine.js
+++ files/scripts/searchEngine.js
@@ -26,6 +26,7 @@ function populateSearch(){
     for(i=0; i<searchWords.length; i++){
         const srchWrd = searchWords[i].toLowerCase(); // the word we're searching for in the site index
 
+        searchResults.innerHTML += "<div id='webpageResultsBin' style='display:none'><h2>Webpage Results</h2></div>";
         // Search Website Index
         for(k=0; k<websiteIndex.length; k++){
             const itemTitleArray = websiteIndex[k].title.toLowerCase().split(' '); // the title of the current item in the index
@@ -36,24 +37,30 @@ function populateSearch(){
             if(itemTitleArray.includes(srchWrd) || itemDescArray.includes(srchWrd) || itemTagsArray.includes(srchWrd)){
                 // create a new div for the search result
                 
-                if(itemsList.includes(k) == false){ // if the item is not already in the list of items to display
+                if(itemsList.includes('siteIndex-'+k) == false){ // if the item is not already in the list of items to display
 
                     if(websiteIndex[k].link.includes('[external]')){
                         websiteIndex[k].link = websiteIndex[k].link.replace('[external]', ''); // remove the [external] tag from the link
                     }
 
-                    searchResults.innerHTML += "<div id='siteIndex-"+k+"'><h3 style='margin-bottom:5px;'><a href='"+websiteIndex[k].link+"'>"+websiteIndex[k].title+"</a></h3>"+websiteIndex[k].description+"<p style='margin-top:5px; color:dimgray;'>"+itemTagsArray+"</p></div>";
-                    itemsList.push(k); // add the item to the list of items to display
+                    document.getElementById('webpageResultsBin').innerHTML += "<div id='siteIndex-"+k+"'><h3 style='margin-bottom:5px;color:midnightblue;'><a href='"+websiteIndex[k].link+"'>"+websiteIndex[k].title+"</a></h3>"+websiteIndex[k].description+"<p style='margin-top:5px; color:dimgray;'>"+itemTagsArray+"</p></div>";
+                    itemsList.push('siteIndex-'+k); // add the item to the list of items to display
                     resultsCount += 1;
+
+                    if(document.getElementById('webpageResultsBin').style.display == 'none'){
+                        document.getElementById('webpageResultsBin').style.display = 'block'; // show the search results
+                    }
                 }
             }
         }
 
-        itemsList.length = 0; // clear the list of items displayed
+        //itemsList.length = 0; // clear the list of items displayed
 
+        
         // Search Events
+        searchResults.innerHTML += "<div id='eventResultsBin' style='display:none'><br><h2>Resort Events Results</h2></div>";
         for(k=0; k<eventlistArray.length; k++){
-            if(itemsList.includes(k) == false){ // if the item is not already in the list of items to display
+            if(itemsList.includes('event-'+k) == false){ // if the item is not already in the list of items to display
                 let temp = eventlistArray[k].split('|'); // split the event into its components
                 const eventName = temp[1].toLowerCase().split(' '); // the name of the event
                 var eventDescription;
@@ -67,23 +74,28 @@ function populateSearch(){
                 if(eventName.includes(srchWrd) || eventDescription.includes(srchWrd)){
                     // create a new div for the search result
                     if(temp[4].includes('none') == false){ // if the event has a link
-                        searchResults.innerHTML += "<div id='event-"+k+"'><h3 style='margin-bottom:5px;'><a href='"+temp[4]+"'>"+temp[1]+"</a></h3>"+temp[5].replace('yes/', '')+"</div>";
+                        document.getElementById('eventResultsBin').innerHTML += "<div id='event-"+k+"'><h3 style='margin-bottom:5px;color:midnightblue;'><a href='"+temp[4]+"'>"+temp[1]+"</a></h3>"+temp[5].replace('yes/', '')+"</div>";
                         resultsCount += 1;
-                        itemsList.push(k); // add the item to the list of items to display
+                        itemsList.push('event-'+k); // add the item to the list of items to display
                     }else{
-                        searchResults.innerHTML += "<div id='event-"+k+"'><h3 style='margin-bottom:5px;'>"+temp[1]+"</h3>"+eventDescription+"</div>";
+                        document.getElementById('eventResultsBin').innerHTML += "<div id='event-"+k+"'><h3 style='margin-bottom:5px;color:midnightblue;'>"+temp[1]+"</h3>"+eventDescription+"</div>";
                         resultsCount += 1;
-                        itemsList.push(k); // add the item to the list of items to display
+                        itemsList.push('event-'+k); // add the item to the list of items to display
+                    }
+
+                    if(document.getElementById('eventResultsBin').style.display =='none'){
+                        document.getElementById('eventResultsBin').style.display = 'block'; // show the search results
                     }
                 }
             }
         }
 
-        itemsList.length = 0; // clear the list of items displayed
+        //itemsList.length = 0; // clear the list of items displayed
 
         // Search Restaurants
+        searchResults.innerHTML += "<div id='diningResultsBin' style='display:none'><br><h2>Restaurants & Dining</h2></div>";
         for(k=0; k<diningArray.length; k++){
-            if(itemsList.includes(k) == false){ // if the item is not already in the list of items to display
+            if(itemsList.includes('dining-'+k) == false){ // if the item is not already in the list of items to display
                 let temp = diningArray[k].split(' | '); // split the event into its components
                 const diningName = temp[0].toLowerCase().split(' '); // the name of the restaurant
                 var diningDescription = temp[5].toLowerCase().split(' '); // the description of the restaurant;
@@ -91,16 +103,53 @@ function populateSearch(){
                 // check if the search word is in the event name or description
                 if(diningName.includes(srchWrd) || diningDescription.includes(srchWrd)){
                     // create a new div for the search result
-                    searchResults.innerHTML += "<div id='dining-"+k+"'><h3 style='margin-bottom:5px;'><a href='things-to-do/dining/page?name="+temp[1]+"'>"+temp[0]+"</a></h3>"+temp[5]+"</div>";
+                    document.getElementById('diningResultsBin').innerHTML += "<div id='dining-"+k+"'><h3 style='margin-bottom:5px;color:midnightblue;'><a href='things-to-do/dining/page?name="+temp[1]+"'>"+temp[0]+"</a></h3>"+temp[5]+"</div>";
                     resultsCount += 1;
-                    itemsList.push(k); // add the item to the list of items to display
+                    itemsList.push('dining-'+k); // add the item to the list of items to display
+
+                    if(document.getElementById('diningResultsBin').style.display =='none'){
+                        document.getElementById('diningResultsBin').style.display = 'block'; // show the search results
+                    }
                 }
             }
         }
 
+        //itemsList.length = 0; // clear the list of items displayed
+
+
+        // Search Blog Posts
+        searchResults.innerHTML += "<div id='blogResultsBin' style='display:none'><br><h2>Blog Posts & Operations Updates</h2></div>";
+        for(k=1; k<blogHeadings.length; k++){
+            if(itemsList.includes('blog-'+k) == false){ // if the item is not already in the list of items to display
+                let temp = blogHeadings[k].split(' | '); // split the event into its components
+                const blogName = temp[0].toLowerCase().split(' '); // contents of blog heading / title
+                const blogCategory = blogCategories[temp[2]].toLowerCase().split(' '); // the category of the blog post
+                var blogAuthor = temp[3].toLowerCase().split(' '); // the author of the blog
+
+                // check if the search word is in the event name or description
+                if(blogName.includes(srchWrd) || blogCategory.includes(srchWrd) || blogAuthor.includes(srchWrd)){
+                    // create a new div for the search result
+                    document.getElementById('blogResultsBin').innerHTML += "<div id='blog-"+k+"'><h3 style='margin-bottom:5px; color:midnightblue;'><a href=''>Blog Post: "+temp[0]+"</a></h3>"+blogCategories[temp[2]]+" - " + temp[3] + "</div>";
+                    resultsCount += 1;
+                    itemsList.push('blog-'+k); // add the item to the list of items to display
+
+                    if(document.getElementById('blogResultsBin').style.display =='none'){
+                        document.getElementById('blogResultsBin').style.display = 'block'; // show the search results
+                    }
+                }
+            }
+        }
+        
     }
 
-    document.getElementById("resultsCountDisplay").innerHTML = "Showing <b>" + resultsCount + "</b> results";
+    
+
+    if(resultsCount == 0){
+        document.getElementById("resultsCountDisplay").innerHTML = "No results found";
+        searchResults.innerHTML = "<p>We could not find any pages containing the keywords. Consider searching for just 1 - 2 keywords, avoid using special characters or punctuation.</p>";
+    }else{
+        document.getElementById("resultsCountDisplay").innerHTML = "Showing <b>" + resultsCount + "</b> results";
+    }
 } // end of populateSearch()
 
 function resetSearchBar(){
